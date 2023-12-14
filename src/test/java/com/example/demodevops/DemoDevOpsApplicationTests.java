5b55d1d87428a2b394c7ac16624286ac3946b904
Dương Song <duongsong9a4@gmail.com>
2023-12-14 22:21:19 +0700

    first commit

---
+++ src/test/java/com/example/demodevops/DemoDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demodevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDevOpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
